4033ce5a4163aa5b89eca2f510a64aa6207908d5
NikaBidzo22 <118376908+NikaBidzo22@users.noreply.github.com>
2023-05-22 12:30:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const output = document.querySelector(".main-output");
+const inputs = document.querySelectorAll(".input");
+const submitBtn = document.querySelector(".submit")
+const thank = document.querySelector(".second")
+const rate = document.querySelector(".first")
+const sorry = document.querySelector(".long")
+
+let storeValue = null;
+
+console.log(inputs);
+
+inputs.forEach(function (input){
+    input.addEventListener('click' , function(event) {        
+        storeValue = event.target.innerText
+        inputs.forEach(function (inputTwo) {
+            if (storeValue == inputTwo.innerText) {
+                inputTwo.classList.add("white");
+                output.innerHTML = storeValue;
+            } else {
+                inputTwo.classList.remove("white");
+            }
+        });
+    });
+});
+
+submitBtn.addEventListener('click' , function() {
+    thank.style.display = "flex"
+    rate.style.display = "none"
+});
