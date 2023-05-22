401764235de6df741c80f9339ebb273e34c59507
NikaBidzo22 <118376908+NikaBidzo22@users.noreply.github.com>
2023-05-22 14:17:20 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-
 const output = document.querySelector(".main-output");
 const inputs = document.querySelectorAll(".input");
-const submitBtn = document.querySelector(".submit")
-const thank = document.querySelector(".second")
-const rate = document.querySelector(".first")
-const sorry = document.querySelector(".long")
+const submitBtn = document.querySelector(".submit");
+const thank = document.querySelector(".second");
+const rate = document.querySelector(".first");
+const sorry = document.querySelector(".long");
+const change = document.querySelector(".you")
 
 let storeValue = null;
 
 console.log(inputs);
 
-inputs.forEach(function (input){
-    input.addEventListener('click' , function(event) {        
-        storeValue = event.target.innerText
-        inputs.forEach(function (inputTwo) {
-            if (storeValue == inputTwo.innerText) {
-                inputTwo.classList.add("white");
-                output.innerHTML = storeValue;
-            } else {
-                inputTwo.classList.remove("white");
-            }
-        });
+inputs.forEach(function (input) {
+  input.addEventListener("click", function (event) {
+    storeValue = event.target.innerText;
+    inputs.forEach(function (inputTwo) {
+      if (storeValue == inputTwo.innerText) {
+        inputTwo.classList.add("white");
+        output.innerHTML = storeValue;
+        change.innerHTML = "Thank you!"
+      } else {
+        inputTwo.classList.remove("white");
+      }
     });
+  });
 });
 
-submitBtn.addEventListener('click' , function() {
-    thank.style.display = "flex"
-    rate.style.display = "none"
+submitBtn.addEventListener("click", function () {
+  thank.style.display = "flex";
+  rate.style.display = "none";
 });
